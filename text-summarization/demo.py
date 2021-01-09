c79812f9a667b25b1ef9882ba06213cef7d11ec6
zhuzihan728 <zcabhub@ucl.ac.uk>
2021-01-09 09:05:05 +0000

    update demo

--- text-summarization/demo.py
+++ text-summarization/demo.py
@@ -9,22 +9,35 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
-
+import os
+import read_file
 
 LANGUAGE = "english"
-SENTENCES_COUNT = 10
 
-
-if __name__ == "__main__":
-    url = "https://en.wikipedia.org/wiki/Automatic_summarization"
-    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+def summary(path):
+    # url = "https://en.wikipedia.org/wiki/Automatic_summarization"
+    # parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    content = ""
+    count = read_file.readPdf(path,"a.txt") 
+    line = min(20,count*5) 
+    content += "The document is summarized in " + str(line) + " sentences:\n"
     # or for plain text files
-    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+    parser = PlaintextParser.from_file("a.txt", Tokenizer(LANGUAGE))
     # parser = PlaintextParser.from_string("Check this out.", Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
 
     summarizer = Summarizer(stemmer)
     summarizer.stop_words = get_stop_words(LANGUAGE)
 
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+    for sentence in summarizer(parser.document, line):
+        content += str(sentence) + "\n"
+        content += " " + "\n"
+    os.remove("a.txt")
+    return content
+
+
+if __name__ == "__main__":
+    content = summary("text-summarization/text.pdf")
+    print (content)
+    
+
