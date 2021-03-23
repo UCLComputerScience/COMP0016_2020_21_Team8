020ce7a7fdd5a704ac500401b689f0cc6fbac591
zhuzihan728 <zcabhub@ucl.ac.uk>
2021-03-23 05:19:20 +0000

    deletion

--- AvabotTeam8-src/APItest/query_test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://20.77.57.60:5000?query=what is randoop?')
-    .then(function (v) {
-        console.log('answer: ' + v.data.answer + '\n' + 'context: ' + v.data.context);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
