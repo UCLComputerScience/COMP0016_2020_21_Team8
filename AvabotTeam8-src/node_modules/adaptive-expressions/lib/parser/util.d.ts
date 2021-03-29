cac5678f9500e9788517d8cd28ff95da1d1d2334
zhuzihan728 <zcabhub@ucl.ac.uk>
2021-03-29 04:35:21 +0100

    add node module

---
+++ AvabotTeam8-src/node_modules/adaptive-expressions/lib/parser/util.d.ts
@@ -0,0 +1,19 @@
+/**
+ * @module adaptive-expressions
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+/**
+ * util class
+ */
+export declare class Util {
+    /**
+     * trim char
+     * @param str input string
+     * @param char trim character
+     */
+    static trim(str: string, char: string): string;
+}
+//# sourceMappingURL=util.d.ts.map
