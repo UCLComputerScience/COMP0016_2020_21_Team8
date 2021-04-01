20715e01508a51a3c0b25be109eb212ac366247b
zhuzihan728 <55835587+zhuzihan728@users.noreply.github.com>
2021-04-01 07:14:42 +0100

    Update endpoint.py

--- formRecogFunction/FormRecogFunction/endpoint.py
+++ formRecogFunction/FormRecogFunction/endpoint.py
@@ -1,5 +1,5 @@
-ENDPOINT = "https://formrecognizerteam8.cognitiveservices.azure.com/"
-KEY = 'dcadf9e105944057ae4b851df205f479'
+ENDPOINT = 
+KEY = 
 
-STOREAGE_ACCOUNT_KEY = "XvK2pwC5IsLXMhJ9aLyhaFH49DpFANSJVXHhqtFQrMi7wYa/AvvsdGLgyRruIdRqdfCopXsZnmqaxZKop1+fIw=="
-STOREAGE_ACCOUNT_NAME = 'storageaccountteam8'
+STOREAGE_ACCOUNT_KEY = 
+STOREAGE_ACCOUNT_NAME = 
